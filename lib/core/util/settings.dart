a0a7203ba165a1dca81a5d6ce36ddcdf88766f31
eslamabotaleb07 <eslamabotaleb07@gmail.com>
2025-01-17 16:33:11 +0200

    preapre function to check user still logged in

--- lib/core/util/settings.dart
+++ lib/core/util/settings.dart
@@ -1,2 +1,11 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
 const BASE_URL = "https://dummyjson.com";
 const String SERVER_FAILURE_MESSAGE = 'Please try again later .';
+
+const FlutterSecureStorage secureStorage = FlutterSecureStorage();
+Future<bool> checkUserLoggedIn() async {
+  // Retrieve the token from secure storage
+  final String? token = await secureStorage.read(key: 'accessToken');
+  return token != null && token.isNotEmpty; // User is logged in if token exists
+}
