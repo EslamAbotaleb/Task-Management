171008967046c30c6e8ceef0d8530ea819356f12
eslamabotaleb07 <eslamabotaleb07@gmail.com>
2025-01-17 16:08:42 +0200

    define auth event

--- lib/modules/authentication/presentation/bloc/auth/bloc/auth_event.dart
+++ lib/modules/authentication/presentation/bloc/auth/bloc/auth_event.dart
@@ -6,3 +6,20 @@ sealed class AuthEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+final class LoginEvent extends AuthEvent {
+  final String userName;
+  final String password;
+
+  const LoginEvent({required this.userName, required this.password});
+
+  @override
+  List<Object> get props => [userName, password];
+}
+
+final class LogoutEvent extends AuthEvent {
+  const LogoutEvent();
+
+  @override
+  List<Object> get props => [];
+}
