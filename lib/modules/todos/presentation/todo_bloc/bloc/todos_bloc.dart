a95b7033fc5746788895f5c97e484fff39aae109
eslamabotaleb07 <eslamabotaleb07@gmail.com>
2025-01-17 20:36:33 +0200

    define todos event

--- lib/modules/todos/presentation/todo_bloc/bloc/todos_bloc.dart
+++ lib/modules/todos/presentation/todo_bloc/bloc/todos_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:task_mangement/modules/todos/domain/entities/todo_entity.dart';
 
 part 'todos_event.dart';
 part 'todos_state.dart';
--- lib/modules/todos/presentation/todo_bloc/bloc/todos_event.dart
+++ lib/modules/todos/presentation/todo_bloc/bloc/todos_event.dart
@@ -6,3 +6,13 @@ sealed class TodosEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class GetAllTodoEvent extends TodosEvent {}
+
+class CheckIfNeedMoreTodoEvent extends TodosEvent {
+  final int index;
+  const CheckIfNeedMoreTodoEvent({required this.index});
+
+  @override
+  List<Object> get props => [index];
+}
