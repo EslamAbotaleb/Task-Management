38aa74ca9f0222908a2b7b7a94cda51aee4d2c5d
eslamabotaleb07 <eslamabotaleb07@gmail.com>
2025-01-17 20:28:14 +0200

    define implementation repository

--- lib/modules/todos/data/repositories/todo_repository_implement.dart
+++ lib/modules/todos/data/repositories/todo_repository_implement.dart
@@ -1,9 +1,20 @@
 import 'package:dartz/dartz.dart';
 import 'package:task_mangement/core/error/failures.dart';
+import 'package:task_mangement/modules/todos/data/datasource/remote/abstract/todo_abstract_remote.dart';
+import 'package:task_mangement/modules/todos/data/datasource/remote/todo_remote_data_source.dart';
 import 'package:task_mangement/modules/todos/domain/entities/todo_entity.dart';
 import 'package:task_mangement/modules/todos/domain/repositories/todo_repository.dart';
 
+import '../../../../core/error/exceptions.dart';
+import '../../../../core/network/abstract/network_abstract.dart';
+
 class TodoRepositoryImplement implements TodoRepository {
+  final TodoRemoteDataSource remoteDataSource;
+  final NetworkInfo networkInfo;
+
+  TodoRepositoryImplement(
+      {required this.remoteDataSource, required this.networkInfo});
+
   @override
   Future<Either<Failure, Unit>> addTodo(TodoEntity todo) {
     // TODO: implement addTodo
@@ -17,15 +28,26 @@ class TodoRepositoryImplement implements TodoRepository {
   }
 
   @override
-  Future<Either<Failure, List<TodoEntity>>> getTodosByPaginate({required int skip, required int limit}) {
-    // TODO: implement getTodosByPaginate
-    throw UnimplementedError();
-  }
-
-  @override
   Future<Either<Failure, Unit>> updateTodo(TodoEntity todo) {
     // TODO: implement updateTodo
     throw UnimplementedError();
   }
-  
+
+  @override
+  Future<Either<Failure, List<TodoEntity>>> getTodosByPaginate(
+      {required int pageNumber, required int limit}) async {
+    if (await networkInfo.isConnected) {
+      try {
+        final result = await remoteDataSource.getTodosByPaginate(
+          pageNumber: pageNumber,
+          limit: limit,
+        );
+        return Right(result);
+      } on ServerException {
+        return Left(ServerFailure());
+      }
+    } else {
+      return Left(ServerFailure());
+    }
+  }
 }
--- lib/modules/todos/domain/repositories/todo_repository.dart
+++ lib/modules/todos/domain/repositories/todo_repository.dart
@@ -5,7 +5,7 @@ import 'package:task_mangement/modules/todos/domain/entities/todo_entity.dart';
 abstract class TodoRepository {
   // get todos by pagination
   Future<Either<Failure, List<TodoEntity>>> getTodosByPaginate(
-      {required int skip, required int limit});
+      {required int pageNumber, required int limit});
   // Mark: - (Create, Delete, Update)
   Future<Either<Failure, Unit>> deleteTodo(int id);
   Future<Either<Failure, Unit>> updateTodo(TodoEntity todo);
