c8ebeccaad2ff9b5072acebdccaf1b54ddb1a9b1
eslamabotaleb07 <eslamabotaleb07@gmail.com>
2025-01-17 17:22:09 +0200

    Update Ui Login page

--- lib/modules/authentication/presentation/screens/form_login_widget.dart
+++ lib/modules/authentication/presentation/screens/form_login_widget.dart
@@ -1,27 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:task_mangement/core/theme/app_theme.dart';
 
 import '../bloc/auth/bloc/auth_bloc.dart';
 
-class FormLoginWidget extends StatelessWidget {
+class FormLoginWidget extends StatefulWidget {
   FormLoginWidget({super.key});
 
-  final TextEditingController _usernameController = TextEditingController();
+  @override
+  State<FormLoginWidget> createState() => _FormLoginWidgetState();
+}
 
-  final TextEditingController _passwordController = TextEditingController();
+class _FormLoginWidgetState extends State<FormLoginWidget> {
+  final TextEditingController _usernameController = TextEditingController(text: 'emilys');
+
+  final TextEditingController _passwordController = TextEditingController(text: 'emilyspass');
 
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  final spacing = 16.0;
+  bool _obscureText = true;
 
   @override
   Widget build(BuildContext context) {
     return Form(
       key: _formKey,
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           TextFormField(
             controller: _usernameController,
-            decoration: const InputDecoration(labelText: 'Username'),
+            // initialValue: 'emilys',
+            // decoration: const InputDecoration(labelText: 'Username'),
+            decoration: InputDecoration(
+              labelText: 'Username',
+              prefixIcon: const Icon(Icons.person),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
             validator: (value) {
               if (value == null || value.isEmpty) {
                 return 'Please enter your username';
@@ -29,10 +47,31 @@ class FormLoginWidget extends StatelessWidget {
               return null;
             },
           ),
+          SizedBox(
+            height: spacing,
+          ),
           TextFormField(
             controller: _passwordController,
-            obscureText: true,
-            decoration: const InputDecoration(labelText: 'Password'),
+            // initialValue: 'emilyspass',
+             obscureText: _obscureText,
+            decoration: InputDecoration(
+              labelText: 'Password',
+              prefixIcon: const Icon(Icons.password),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+              suffixIcon: IconButton(
+                icon: Icon(
+                  _obscureText ? Icons.visibility_off : Icons.visibility,
+                ),
+                onPressed: () {
+                  setState(() {
+                    _obscureText = !_obscureText;
+                  });
+                },
+              ),
+            ),
+            // decoration: const InputDecoration(labelText: 'Password'),
             validator: (value) {
               if (value == null || value.isEmpty) {
                 return 'Please enter your password';
@@ -40,7 +79,12 @@ class FormLoginWidget extends StatelessWidget {
               return null;
             },
           ),
-          const SizedBox(height: 20),
+          SizedBox(
+            height: spacing,
+          ),
+          SizedBox(
+            height: spacing,
+          ),
           ElevatedButton(
             onPressed: () {
               if (_formKey.currentState?.validate() ?? false) {
@@ -52,7 +96,23 @@ class FormLoginWidget extends StatelessWidget {
                 );
               }
             },
-            child: const Text('Login'),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: primaryColor, // Button color
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8), // Rounded corners
+              ),
+              padding: const EdgeInsets.symmetric(
+                  vertical: 16, horizontal: 32), // Padding
+              textStyle: const TextStyle(
+                fontSize: 18, // Text size
+                fontWeight: FontWeight.bold, // Bold text
+              ),
+              elevation: 8, // Shadow effect
+            ),
+            child: const Text(
+              'Login',
+              style: TextStyle(color: Colors.white),
+            ),
           ),
         ],
       ),
--- lib/modules/authentication/presentation/screens/login_page.dart
+++ lib/modules/authentication/presentation/screens/login_page.dart
@@ -1,19 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:task_mangement/modules/authentication/presentation/screens/body_login_widget.dart';
 
-class LoginPage extends StatefulWidget {
+class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
-  @override
-  State<LoginPage> createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Login', style: TextStyle(color: Colors.white),),
+        title: const Text(
+          'Login',
+          style: TextStyle(color: Colors.white),
+        ),
       ),
       body: const BodyLoginWidget(),
     );
