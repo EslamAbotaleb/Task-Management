a34e3b58fe150287f909831b12b5d0320aabd8c5
eslamabotaleb07 <eslamabotaleb07@gmail.com>
2025-01-17 16:06:05 +0200

    prepare the auth state

--- lib/modules/authentication/presentation/bloc/auth/bloc/auth_bloc.dart
+++ lib/modules/authentication/presentation/bloc/auth/bloc/auth_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:task_mangement/modules/authentication/domain/entities/login_entity.dart';
 
 part 'auth_event.dart';
 part 'auth_state.dart';
