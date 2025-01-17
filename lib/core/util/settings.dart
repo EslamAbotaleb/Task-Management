dae1effafb31f3281ba2aa13d6a0d03de9033ffc
eslamabotaleb07 <eslamabotaleb07@gmail.com>
2025-01-17 17:02:42 +0200

    define logger to be easy in printing format

--- lib/core/util/settings.dart
+++ lib/core/util/settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:logger/logger.dart';
 
 const BASE_URL = "https://dummyjson.com";
 const String SERVER_FAILURE_MESSAGE = 'Please try again later .';
@@ -9,3 +10,12 @@ Future<bool> checkUserLoggedIn() async {
   final String? token = await secureStorage.read(key: 'accessToken');
   return token != null && token.isNotEmpty; // User is logged in if token exists
 }
+
+Logger logger = Logger(
+  printer: PrettyPrinter(
+    printTime: true,
+    methodCount: 1,
+    errorMethodCount: 2,
+    colors: false,
+  ),
+);
